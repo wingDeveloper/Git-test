453e34f87a0c688eb56e4d3f612b95ba9acad740
markup <markup@192.168.1.254>
2016-05-16 22:32:44 +0300

    New line

--- Git-test-project/src/ua/wing/test/Main.java
+++ Git-test-project/src/ua/wing/test/Main.java
@@ -2,7 +2,7 @@ package ua.wing.test;
 
 public class Main {
 	public static void main(String[] args) {
-		System.out.println("Hello from GitHub!");
+		System.out.println("Hello from GitHub!!!!");
 	}
 
 }
